e793687ba041c6132f37131a44f47ce5e61ced8c
naomi <naomi.robguz@gmail.com>
2025-10-29 10:36:37 -0600

    complete navbar, guest, about, contact- Light/dark themes working

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
 import Image from "next/image";
+import { currentUser } from "@clerk/nextjs/server";
+import Guest from '@/components/Guest';
 
-export default function Home() {
+export default async function HomePage() { //async  to use await inside the function (on the server side)
+  const user = await currentUser();
+
+  if(!user){
+    return <Guest />
+  }
   return (
     <div className='bg-red-400'>HomePage</div>
   )
